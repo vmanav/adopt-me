8d05ecadc5e6f2b8732f7310629b32e0dbc7c0cd
vmanav <zmanav.1999@gmail.com>
2020-04-26 21:12:04 +0530

    Update Pet component, btter styling.

--- src/Pet.js
+++ src/Pet.js
@@ -1,17 +1,34 @@
 import React from 'react';
-const Pet = ({ name, animal, breed }) => {
+const Pet = ({ name, animal, breed, media, location, id }) => {
     // return React.createElement("div", {}, [
     //     React.createElement("h1", {}, name),
     //     React.createElement("h2", {}, animal),
     //     React.createElement("h2", {}, breed),
     // ]);
+    // return (
+    //     <div>
+    //         <h1><i>Name : {name}</i></h1>
+    //         <h2>Animal : {animal}</h2>
+    //         <h2>Breed : {breed}</h2>
+    //     </div>
+    // )
+
+    let hero = 'http://placecorgi.com/300/300';
+    if (media.length) {
+        hero = media[0].small;
+    }
+
     return (
-        <div>
-            <h1><i>Name : {name}</i></h1>
-            <h2>Animal : {animal}</h2>
-            <h2>Breed : {breed}</h2>
-        </div>
-    )
+        <a href={`/details/${id}`} className="pet">
+            <div className="image-container">
+                <img src={hero} alt={name} />
+            </div>
+            <div className="info">
+                <h1>{name}</h1>
+                <h2>{`${animal} — ${breed} — ${location}`}</h2>
+            </div>
+        </a>
+    );
 };
 
 export default Pet;
